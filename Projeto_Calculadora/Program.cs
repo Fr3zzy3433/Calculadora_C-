5ccef812c0655bd418643ac637771ef93c7ab97e
Logan Barcellos <mloganbarcellos@gmail.com>
2023-08-30 00:05:01 -0300

    Primeiro esboço do projeto

--- Projeto_Calculadora/Program.cs
+++ Projeto_Calculadora/Program.cs
@@ -10,6 +10,22 @@ namespace Projeto_Calculadora
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.WriteLine();
+            Console.Write("Qual operação deseja fazer?: ");
+            int operacao = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite o primeiro número: ");
+            int primeiro_num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite o primeiro número: ");
+            int segundo_num = int.Parse(Console.ReadLine());
         }
     }
 }
